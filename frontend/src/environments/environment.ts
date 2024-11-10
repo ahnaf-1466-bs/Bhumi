b46f8999defb3ac8d932b686e459bbe0d34e7df9
ahnaf-1466-bs <ahnaf.mubashshir@brainstation-23.com>
2024-11-10 21:58:22 +0600

    updated gitignore file

---
+++ frontend/src/environments/environment.ts
@@ -0,0 +1,21 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+    baseURL: 'https://api.prod.vumi.com.bd',
+    production: false,
+    wstoken: '76f904e5c44b82fad7789a159819db22',
+    globalToken: 'bf9f6ac46662f119040f711b18ab966d',
+    ZOOM_SDK_KEY: 'cW790Ox0RLS1cDdZdki0rw',
+    ZOOM_SDK_SECRET: 'ePdjK50FFN6CEiaEVDqSBBt9JccJtvF7',
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
